1c581b59e75e0eea8ef478e1abe43c88ae816f23
Vlad Promotorov <vlad.promotorov@studiomobile.ru>
2018-08-13 12:56:22 +0300

    Create MyExRestAPIService

---
+++ MyExRestAPIService/Services/Helpers/NSStringHelper.h
@@ -0,0 +1,13 @@
+//
+//  NSStringHelper.h
+//  MyExRestAPIService
+//
+//  Created by Guest User on 13.08.2018.
+//  Copyright © 2018 Qoden. All rights reserved.
+//
+
+#ifndef NSStringHelper_h
+#define NSStringHelper_h
+
+
+#endif /* NSStringHelper_h */
---
+++ MyExRestAPIService/Services/Helpers/NSStringHelper.m
@@ -0,0 +1,9 @@
+//
+//  NSStringHelper.m
+//  MyExRestAPIService
+//
+//  Created by Guest User on 13.08.2018.
+//  Copyright © 2018 Qoden. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
